b39dbe70d93bf03589e0862ef93a0c96db235641
alokg-812 <alokg7055@gmail.com>
2024-02-21 16:14:25 +0530

    data_Types_declaration

---
+++ data_declaring.js
@@ -0,0 +1,18 @@
+let a = 20;
+let b = "ALok";
+let c = true;
+let d;
+console.log(a,b,c,d);
+d = 28
+console.log(d);
+b = 8;
+console.log(b);
+// ARRAYS-
+arr = [25,'Alok', true, 'ABC'];
+console.log(arr[3]);
+let e = {
+    name : 'ALok',
+    contact : 7752953990,
+    mail_id : 'alokg7055'
+};
+console.log(e.contact);
