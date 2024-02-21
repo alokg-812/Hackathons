5dc91dc0e688c8df4178139dc0623f4c746613f6
alokg-812 <alokg7055@gmail.com>
2024-02-21 19:45:54 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,26 @@
+//for loop
+for (let i = 0; i < 10 ; i++) {
+    console.log(i+1,"ALok");
+}
+
+//while loop
+let j = 0;
+while(j<10) {
+    console.log(j+1, "Alok");
+    j += 1;
+}
+
+//do_while loop
+let i = 0;
+do {
+    console.log("Hello",i);
+    i++ ;
+}
+while (i<10);
+
+// let i = 0;
+// do {
+//     console.log("Hello",i);
+// }
+// while (i<10);
+// i++ ;  //this will lead to infinite loop because its syntax of do_while loop that increament decreament statement is written only before the while loop.
