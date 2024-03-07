dc299ae05672b06e673c7dddbbb3a3b1dc3522ef
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-03-07 08:50:20 +0530

    Math inbuilt function

---
+++ math_function.js
@@ -0,0 +1,8 @@
+const pi = Math.PI;
+// console.log(pi);  // 3.1415...
+// console.log(Math.round(pi))  //3 
+// console.log(Math.round(9.81))  //10 
+// console.log(Math.floor(pi))    //3
+// console.log(Math.ceil(pi))    //4
+// console.log(Math.random());   //from 0 to 0.999
+console.log(Math.round(Math.random()*11));
