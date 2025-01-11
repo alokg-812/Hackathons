a73befca10af463ba1ac432e5d61a7727fc0594e
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-11 22:27:07 +0530

    Delete string_Methods.js

--- string_Methods.js
+++
@@ -1,8 +0,0 @@
-// let a = 2;
-// let b = 3;
-//console.log(`The sum of ${a} and ${b} is ${a+b}`);
-let firstName = 'Anurag Tiwari'
-//console.log(firstName.length)
-console.log(firstName.substr(4,5))
-console.log(firstName.substring(4,5))
-console.log(firstName.split(''));
